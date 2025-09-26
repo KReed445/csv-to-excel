a781f6c1a0ada68671f3485f24224d95baa4f770
KReed33 <ortizderek33@gmail.com>
2025-09-25 20:32:32 -0400

    Added more functionality to the program.
    The program now opens a dialogue box (file explorer) and the user gets
    to choose what file they want to convert and then another dialogue box
    where the user chooses where they want to save the file instead of
    having to explicitly write the filepath down in the program.

--- csv_convert.py
+++ csv_convert.py
@@ -1,22 +1,42 @@
 #This is a simple Python program for converting csv files into Excel files. 
 #Created this for general purpose or incase you need to use an Excel but dont have the option to convert it from csv
 #Proposed changes:
-# - opening a dialogue box and choose what file you want to convert (Instead of writing it directly)
+#Make into an executable? 
+#Finished:
+# - opening a dialogue box and choose what file you want to convert (Instead of writing it directly) DONE!
+# - Open a dialogue box and choose where to save said file. DONE!!
 
 import csv
 import openpyxl
+from tkinter import Tk
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
 
+Tk().withdraw()
+
+
+#Opens dialogue box and user chooses what file to convert
+csvFile = askopenfilename()
+
+#Array for saving csv data
 csv_data = []
-#Choose what CSV you want to convert
-with open('./CSV/YOUR_CSV_FILE.csv') as file_obj:
-    reader = csv.reader(file_obj, delimiter=';') #Delimiter default is ',' so must change to what the file uses.
+
+#Program reads and appends the csv data into an array
+with open(csvFile) as file_obj:
+    reader = csv.reader(file_obj, delimiter=',') #Delimiter default is ',' so must change to what the file uses.
     for row in reader:
         csv_data.append(row)
-#Reads the CSV and then populates a new excel workbook.
+
+#Creates and populates a new excel workbook.
 wb = openpyxl.Workbook()
 sheet = wb.active
 for row in csv_data:
     sheet.append(row)
 
-#Save location (File path) of the newly converted file
-wb.save('./Excel/NEW_EXCEL_FILE.xlsx')
+#Save location and file name of the newly converted excel file
+excelFile = filedialog.asksaveasfilename(
+    defaultextension = ".xlsx",
+    filetypes=[("Excel files", "*.xlsx"), ("All files", "*.*")]
+)
+if excelFile:
+    wb.save(excelFile)
